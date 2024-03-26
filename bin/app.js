972f619ac98b5fda4006770d01fe2219c3a22529
Black <blackj@mega7.fun>
2024-03-26 15:04:18 +0800

    chore: ignore bin change

--- bin/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const command_1 = __importDefault(require("./util/command"));
-(0, command_1.default)();
